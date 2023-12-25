d0f9b5f83d299524cbf83b831395be3060f3b20b
Aakash Dhingra <54613654+aakash-dhingra@users.noreply.github.com>
2023-12-26 02:40:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function moveNoButton() {
+    const noButton = document.querySelector('.no-btn');
+    noButton.style.position = 'absolute';
+    const randomX = Math.random() * (window.innerWidth - 100);
+    const randomY = Math.random() * (window.innerHeight - 50);
+    noButton.style.left = `${randomX}px`;
+    noButton.style.top = `${randomY}px`;
+  }
+
+  document.querySelector('.yes-btn').addEventListener('click', function () {
+    const celebrationOverlay = document.querySelector('.celebration-overlay');
+    celebrationOverlay.style.display = 'flex';
+  
+    // Add balloons
+    const balloons = document.querySelector('.balloons');
+    for (let i = 0; i < 10; i++) {
+      const balloon = document.createElement('div');
+      balloon.className = 'balloon';
+      balloons.appendChild(balloon);
+    }
+  
+    // Add confetti
+    const confettiContainer = document.querySelector('.confetti-container');
+    for (let i = 0; i < 100; i++) {
+      const confetti = document.createElement('div');
+      confetti.className = 'confetti';
+      confetti.style.left = Math.random() * 100 + 'vw';
+      confetti.style.animationDelay = Math.random() + 's';
+      confettiContainer.appendChild(confetti);
+    }
+  
+    setTimeout(function () {
+      celebrationOverlay.style.display = 'none';
+      balloons.innerHTML = '';
+      confettiContainer.innerHTML = '';
+    }, 5000); // Change the time (in milliseconds) as desired for how long the celebration lasts
+  });
+  
